2d308ad3b563e74ab0d68ffb5776fbb309c83992
Lee Lott <llott88@gmail.com>
2025-04-07 20:17:37 -0500

    #1 - Renamed Directory Service class and User model.

---
+++ src/Utilities.Authentication/Contracts/IDirectoryServiceUser.cs
@@ -0,0 +1,15 @@
+﻿namespace Utilities.Authentication.Contracts;
+
+public interface IDirectoryServiceUser
+{
+	bool DoesUserExist();
+	string GetUserName();
+	string GetFirstName();
+	string GetLastName();
+	string GetMiddleName();
+	string GetDisplayName();
+	string GetEmailAddress();
+	string GetTelephoneNumber();
+	bool IsAccountLockedOut();
+	List<string> GetGroups();
+}
